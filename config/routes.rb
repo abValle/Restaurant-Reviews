286cfc153a88bae6ae23cbc09ac4cb2911d7f1b4
Allan Valle <allanvalle@outlook.com>
2021-10-12 19:11:18 -0300

    Rest View

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  # Collection
+  # Member
+  resources :restaurants do
+    # Starts with /restaurants
+    collection do # Criar rotas alternativas dentro de um contexto, sem necessidade de ID
+      # get 'restaurants/top', to: 'restaurants#top', as: :top_restaurants
+      get :top
+    end
+    member do # Criar rotas alternativas dentro de um contexto, necessario ID
+      get :chef
+    end
+    resources :reviews, only: %i[new create]
+  end
+  resources :reviews, only: %i[destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,8 @@ Restaurant.destroy_all
   restaurant = Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
-    rating: rand(1..5)
+    rating: rand(1..5),
+    chef_name: Faker::Name.name
   )
   puts "Restaurante id #{restaurant.id}"
 end
